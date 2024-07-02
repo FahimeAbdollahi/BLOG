b35636dc24d6b8049480c3c4a84f5a679c9f5474
FahimeAbdollahi <famiabdollahi@gmail.com>
2024-07-02 23:33:41 +0330

    update post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
 
     protected $fillable = ['title', 'description', 'user_id'];
 
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
