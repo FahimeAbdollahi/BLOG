8a48aced93531f11674049675156b6864be47979
fahime abdollahi <famiabdollahi@gmail.com>
2024-07-03 17:13:52 +0330

    adding comment store route in web.php

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ require __DIR__.'/auth.php';
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit'); // Add edit route
-Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update'); // Add update route
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy'); // Add delete route
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update'); 
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy'); 
+Route::post('/posts/{post}/comments', 'PostController@storeComment')->name('comments.store');
+
